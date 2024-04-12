5d3d1951b7a26a53310c189976c2b07547396280
TsereteliPh <filipptsereteli@yandex.ru>
2024-04-12 17:06:47 +0300

    add and done .book

--- functions.php
+++ functions.php
@@ -117,6 +117,38 @@ function adem_breadcrumbs_indent() {
 	<?php
 }
 
+//TravelLine booking script
+add_action('wp_head', 'adem_travelline_script');
+function adem_travelline_script() {
+	?>
+		<script type='text/javascript'>
+			(function(w) {
+				var q = [
+					['setContext', 'TL-INT-dacha-rezort_2024-03-20', 'ru'],
+					['embed', 'search-form', {
+						container: 'tl-search-form'
+					}],
+				];
+				var h=["ru-ibe.tlintegration.ru","ibe.tlintegration.ru","ibe.tlintegration.com"];
+				var t = w.travelline = (w.travelline || {}),
+				ti = t.integration = (t.integration || {});
+				ti.__cq = ti.__cq? ti.__cq.concat(q) : q;
+				if (!ti.__loader) {
+					ti.__loader = true;
+					var d=w.document,c=d.getElementsByTagName("head")[0]||d.getElementsByTagName("body")[0];
+					function e(s,f) {return function() {w.TL||(c.removeChild(s),f())}}
+					(function l(h) {
+						if (0===h.length) return; var s=d.createElement("script");
+						s.type="text/javascript";s.async=!0;s.src="https://"+h[0]+"/integration/loader.js";
+						s.onerror=s.onload=e(s,function(){l(h.slice(1,h.length))});c.appendChild(s)
+					})(h);
+				}
+			})(window);
+		</script>
+	<?php
+}
+
+
 require 'inc/acf.php';
 require 'inc/load-more.php';
 require 'inc/mail.php';
---
+++ layouts/blocks/book/template.php
@@ -0,0 +1,27 @@
+<section class="book">
+	<div class="container">
+		<?php
+			$title = get_sub_field( 'title' );
+			if ( $title ) :
+				?>
+					<div class="book__title">
+						<svg width="70" height="90"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-logo--frame"></use></svg>
+
+						<?php
+							echo sprintf( '<%1$s class="book__title-text">%2$s</%1$s>',
+								$title['type'],
+								$title['text']
+							);
+						?>
+					</div>
+				<?php
+			endif;
+		?>
+
+		<div id='block-search' class="book__form">
+			<div id='tl-search-form' class='tl-container'>
+				<noindex><a href='https://www.travelline.ru/products/tl-hotel/' rel='nofollow' target='_blank'>TravelLine</a></noindex>
+			</div>
+		</div>
+	</div>
+</section>
